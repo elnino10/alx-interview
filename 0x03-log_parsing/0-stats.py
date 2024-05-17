c398ecf605dbb867b536c8973c83337120cd2690
Joe <joeegboka@gmail.com>
2024-05-17 14:50:41 +0100

    0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -18,8 +18,9 @@ try:
 
             # check if the status code receive exists in the dictionary and
             # increment its count
-            if status_code in status_codes_dict.keys():
-                status_codes_dict[status_code] += 1
+            for key, value in status_codes_dict.items():
+                if status_code == key:
+                    status_codes_dict[key] += 1
 
             # update total size
             total_size += file_size
@@ -29,19 +30,18 @@ try:
 
         if count == 10:
             count = 0  # reset count
-            print('File size: {}'.format(total_size))
+            print(f'File size: {total_size}')
 
             # print out status code counts
             for key, value in sorted(status_codes_dict.items()):
                 if value != 0:
-                    print('{}: {}'.format(key, value))
+                    print(f'{key}: {value}')
 
 except Exception as err:
     pass
 
 finally:
-    print('File size: {}'.format(total_size))
+    print(f'File size: {total_size}')
     for key, value in sorted(status_codes_dict.items()):
         if value != 0:
-            print('{}: {}'.format(key, value))
-
+            print(f'{key}: {value}')
